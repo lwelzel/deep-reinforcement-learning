ff2104512640e8c6225f837fc25640f49afde8d2
lukas_welzel <lukas.leonard.welzel@gmail.com>
2022-03-25 15:45:10 +0100

    Working random buffer

--- A2_DQN/buffer_class.py
+++ A2_DQN/buffer_class.py
@@ -1,36 +1,17 @@
 import numpy as np
 from sys import version_info
 
-class MetaBuffer(np.ndarray):
+class MetaBuffer(object):
     """
     Meta CLass for buffers
     """
     n_args = 5  # s(t), a(t), r(t), s(t+1), done(t+1)
     dtype = np.float64  # might want to change this for discretization
 
-    # from np documentation for subclassing np.ndarrays
-    def __new__(subtype, shape, dtype=float, buffer=None, offset=0,
-                strides=None, order=None, info=None,):
-        obj = super().__new__(subtype, shape, dtype,
-                              buffer, offset, strides, order)
+    def __init__(self, depth=1000, **kwargs):
+        super(MetaBuffer).__init__()
 
-        obj.rng = None
-        obj._step = None
-        obj.buffer_selecter = None
-
-        return obj
-
-    def __array_finalize__(self, obj):
-        if obj is None: return
-        self.rng = getattr(obj, 'rng', None)
-        self._step = getattr(obj, '_step', None)
-        self.buffer_selecter = getattr(obj, 'buffer_selecter', None)
-
-
-    def __init__(self, depth, *kwargs):
-        super(MetaBuffer, self).__init__(shape=(depth, self.__class__.n_args),
-                                         dtype=self.__class__.dtype)
-        self.rng = np.random.default_rng()
+        self._rng = np.random.default_rng()
 
         # meta selection and self-knowledge
         self._step = 0
@@ -38,7 +19,9 @@ class MetaBuffer(np.ndarray):
 
         # buffer
         # s(t), a(t), r(t), s(t+1), done(t+1)
-        self._buffer = np.empty(shape=(depth, 5), dtype=self.__class__.dtype)
+        self._buffer = np.zeros(shape=(depth, self.__class__.n_args), dtype=self.__class__.dtype)
+        print(self._buffer.shape)
+
 
     def __repr__(self):
         return '<%s.%s MetaBuffer object at %s>' % (
@@ -47,7 +30,7 @@ class MetaBuffer(np.ndarray):
             hex(id(self))
         )
 
-    def update_buffer(self, transition=np.empty(shape=(1, 5))):
+    def update_buffer(self, transition=np.ones(shape=5)):
         """
         Updates the replay buffer with the transition.
         The transition is a tuple or np.array of (s(t), a(t), r(t), s(t+1), done(t+1))
@@ -55,13 +38,27 @@ class MetaBuffer(np.ndarray):
 
         :param transition: a tuple of s(t), a(t), r(t), s(t+1), done(t+1)
         """
-        np.put(self._buffer, ind=self._step, v=transition)
-
+        np.put(a=self._buffer,
+               ind=np.arange(self._step * self.__class__.n_args,
+                             self._step * self.__class__.n_args + self.__class__.n_args),
+                             v=transition, mode="wrap")
+        self._step += 1
 
     @classmethod
     def __reset_all__(cls):
         pass
 
     @property
-    def get_buffered(self):
-        return self.buffer_selecter()
+    def sample(self):
+        return self._rng.choice(self._buffer, 1, replace=False, )
+
+
+if __name__ == "__main__":
+    meta_buffer = MetaBuffer(depth=1000)
+
+    for i, transition in enumerate(np.arange(0., 2000. * 5, 1).reshape((2000, 5))):
+        meta_buffer.update_buffer(transition=transition)
+
+    for i in np.arange(2000 * 5, (2000 + 10) * 5, 1).reshape((10, 5)):
+        print(meta_buffer.sample)
+
