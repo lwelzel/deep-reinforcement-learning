35f175a802cd3316af6a1f1d06b79497722faf62
RenVit318 <rens@renskievit.com>
2022-03-25 11:18:39 +0000

    Upload New File

---
+++ A2/helper.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Code based on helper.py by Thomas Moerland for Reinforcement Learning Assignment 1
+
+class LearningCurvePlot:
+
+    def __init__(self,title=None):
+        self.fig,self.ax = plt.subplots()
+        self.ax.set_xlabel('Time')
+        self.ax.set_ylabel('Total Reward')      
+        if title is not None:
+            self.ax.set_title(title)
+        
+    def add_curve(self,y,label=None):
+        ''' y: vector of average reward results
+        label: string to appear as label in plot legend '''
+        if label is not None:
+            self.ax.plot(y,label=label)
+        else:
+            self.ax.plot(y)
+
+    def set_ylim(self,lower,upper):
+        self.ax.set_ylim([lower,upper])
+    
+    def add_hline(self,height,label):
+        self.ax.axhline(height,ls='--',c='k',label=label)
+    
+    def set_yticks(self,yticks): #addition to make y label more detailed
+        self.ax.set_yticks(yticks)
+
+    def save(self,name='test.png'):
+        ''' name: string for filename of saved figure '''
+        self.ax.legend()
+        self.fig.savefig(name,dpi=300)
+
+def smooth(y, window, poly=1):
+    '''
+    y: vector to be smoothed 
+    window: size of the smoothing window '''
+    return savgol_filter(y,window,poly)
+
+def softmax(x, temp):
+    ''' Computes the softmax of vector x with temperature parameter 'temp'.
+        Borrowed from Helper.py from previous Assignment.
+    '''
+    x = x / temp # scale by temperature
+    z = x - max(x) # substract max to prevent overflow of softmax 
+    return np.exp(z)/np.sum(np.exp(z)) # compute softmax
+
+def argmax(x):
+    ''' Own variant of np.argmax with random tie breaking.
+        Borrowed from Helper.py from previous Assignment.
+    '''
+    try:
+        return np.random.choice(np.where(x == np.max(x))[0])
+    except:
+        return np.argmax(x)
+
