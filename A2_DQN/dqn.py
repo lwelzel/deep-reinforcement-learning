2561df57db381bbba56baa244dbda6609c18e6aa
RenVit318 <rens@renskievit.com>
2022-03-29 11:18:48 +0000

    Added tunable learn_dqn() for experiment.py

--- A2_DQN/dqn.py
+++ A2_DQN/dqn.py
@@ -8,15 +8,13 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers, optimizers
 import gym
-from helper import softmax, argmax
 from buffer_class import MetaBuffer
-from helper import LearningCurvePlot, smooth
+from helper import LearningCurvePlot, smooth, softmax, argmax
 
 
 class DeepQAgent:
     def __init__(self, n_inputs,
                  action_space,
-                 depth=2500,
                  learning_rate=0.01,
                  gamma=0.8,
                  hidden_layers=None,
@@ -25,24 +23,26 @@ class DeepQAgent:
                  loss_func='mean_squared_error',
                  use_tn=False,
                  use_er=False,
+                 depth=2500,
+                 sample_batch_size=64
                  ):
 
         self.n_inputs = n_inputs  # can we pull this from somewhere else?
         self.action_space = action_space
         self.learning_rate = learning_rate
         self.gamma = gamma
+        self.hidden_layers = hidden_layers
 
         self.use_tn = use_tn
         self.use_er = use_er
-        
-        self.hidden_layers = hidden_layers
 
-        self.DeepQ_Network = self._initialize_dqn(self.hidden_layers, hidden_act, init, loss_func)
+        self.DeepQ_Network = self._initialize_dqn(hidden_layers, hidden_act, init, loss_func)
         if use_tn:
-            self.Target_Network = self._initialize_dqn(self.hidden_layers, hidden_act, init, loss_func)
+            self.Target_Network = self._initialize_dqn(hidden_layers, hidden_act, init, loss_func)
 
         if use_er:
-            self.buffer = MetaBuffer(depth)
+            self.buffer = MetaBuffer(depth,sample_batch_size)
+
 
     def _initialize_dqn(self, hidden_layers=None, hidden_act='relu', init='HeUniform', loss_func='mean_squared_error'):
         """Template model for both Main and Target Network for the Q-value mapping. Layers should be a list with the number of fully connected nodes per hidden layer"""
@@ -50,7 +50,7 @@ class DeepQAgent:
         model = keras.Sequential()
         model.add(keras.Input(shape=(self.n_inputs,)))
 
-        if hidden_layers == None:
+        if layers == None:
             print("WARNING: No hidden layers given for Neural Network")
             input("Continue? ... ")
         else:
@@ -88,7 +88,7 @@ class DeepQAgent:
             p = softmax(actions, temp)
             a = np.random.choice([0, 1], p=p)
 
-        return a, actions[0]
+        return a
 
     def one_step_update(self, s, a, r, s_next, done):
 
@@ -118,42 +118,120 @@ class DeepQAgent:
             Q_current = self.Target_Network.predict(states)
         else:
             Q_current = self.DeepQ_Network.predict(states)  # Current Q(s,a) values to be updated if a is taken
-
         # TODO: compare with update from last report
         for i in range(0, len(states)):
-            if done_ar[i]:
-                G = rewards[i]  # don't bootstrap
+            if done_ar[i].any():
+                G = rewards[i,0]  # don't bootstrap
             else:
                 next_state = np.reshape(states_next[i], [1, 4])
                 if self.use_tn:
                     max_Q_next = np.max(self.DeepQ_Network.predict(next_state))
                 else:
                     max_Q_next = np.max(self.DeepQ_Network.predict(next_state))
-                G = rewards[i] + self.gamma * max_Q_next
+                G = rewards[i,0] + self.gamma * max_Q_next
 
             # Bellmann's equation
-            Q_current[i, actions[i]] = (1 - self.learning_rate) * Q_current[i, actions[i]] + self.learning_rate * G
+            Q_current[i, int(actions[i,0])] = (1 - self.learning_rate) * Q_current[i, int(actions[i,0])] + self.learning_rate * G
 
         states = np.reshape(states, [len(states), 4])  # reshape to feed into keras
 
         # Fit and train the network
-        # self.DeepQ_Network.fit(states, Q_current, epochs=1, verbose=True)
-
+        self.DeepQ_Network.fit(states, Q_current, epochs=1, verbose=0)
 
     def save(self, rewards):
-        """Saves Deep Q-Network and array of rewards
-        """
-        # self.dir_location = Path(file_location)
-        # self.name = f"run={strftime('%Y-%m-%d-%H-%M-%S', gmtime())}_den={density:.0e}_temp={temperature:.0e}_part={n_particles}_id={id}.h5"
-        # Path(self.dir_location).mkdir(parents=True, exist_ok=True)
-        # self.file_location = Path(file_location) / self.name
+        """Saves Deep Q-Network and array of rewards"""
         model_name = f"{self.hidden_act}-alpha{self.learning_rate}-gamma{self.gamma}-"+"".join([str(n) for n in self.hidden_layers])
         self.DeepQ_Network.save("DeepQN_{}.h5".format(model_name))
         np.savetxt("Rewards_{}.csv".format(model_name), a, delimiter=",")
 
 
-def learn_dqn():
-    """Catch all function containing the complete learning process for a DQN on the gym polecart environment """
+
+def save_run(rewards, agent):
+    # self.dir_location = Path(file_location)
+    # self.name = f"run={strftime('%Y-%m-%d-%H-%M-%S', gmtime())}_den={density:.0e}_temp={temperature:.0e}_part={n_particles}_id={id}.h5"
+    # Path(self.dir_location).mkdir(parents=True, exist_ok=True)
+    # self.file_location = Path(file_location) / self.name
+    pass
+
+
+
+
+def learn_dqn(learning_rate,policy,epsilon,temp,gamma,hidden_layers,use_er,use_tn,num_iterations,depth=2500,learn_freq=4,
+              target_update_freq=25,sample_batch_size=128,render=False):
+    """Callable DQN function for complete runs and parameter optimization"""
+    env = gym.make('CartPole-v1')
+    pi = DeepQAgent(4, env.action_space, learning_rate, gamma, hidden_layers, use_tn=use_tn, use_er=use_er, depth=depth, sample_batch_size=sample_batch_size)
+
+    all_rewards = np.full(shape=num_iterations, fill_value=np.nan, dtype=np.float64) #use to keep track of learning curve
+    
+    #we initialize an empty buffer, fill it in with random values first. Later additions then overwrite these random values
+    if use_er:
+        timesteps = 0
+        while timesteps < depth+1: #+1 to make sure buffer is filled
+            s = env.reset()
+            done = False
+            while not done:
+                a = pi.select_action(s, policy, epsilon, temp)
+                s_next, r, done, _ = env.step(a)
+                pi.buffer.update_buffer(np.array([s,a,r,s_next,done]))
+                timesteps += 1
+                if render: env.render()
+
+    for iter in range(num_iterations):
+        s = env.reset()
+        done = False
+        episode_reward = 0
+
+        while not done:
+            a = pi.select_action(s, policy, epsilon, temp)
+            s_next, r, done , _ = env.step(a)
+            episode_reward += r
+            if pi.use_er:
+                pi.buffer.update_buffer(np.array([s,a,r,s_next,done]))
+
+        
+            else:
+                pi.one_step_update(s,a,r,s_next,done) #one-step 'dumb' update
+            
+            s = s_next
+            if render: env.render()
+
+        all_rewards[iter] = episode_reward
+        if render: print("Iteration {0}: Timesteps survived: {1}".format(iter, int(episode_reward)))
+
+        if pi.use_er:
+            batch = pi.buffer.sample
+            batch = np.asarray(batch).astype('float32')
+            pi.update(batch[:,0],batch[:,1],batch[:,2],batch[:,3],batch[:,4])
+            
+
+        if pi.use_tn and iter % target_update_freq == 0:
+            if render: print("Updating Target Network")
+            pi.Target_Network.set_weights(pi.DeepQ_Network.get_weights())
+
+    #save model and learning curve
+
+
+    env.close()
+    pi.save(all_rewards)
+
+    return all_rewards
+
+            
+    
+    
+            
+    
+    
+    
+
+
+
+
+
+
+def play_dqn():
+    """Catch all function containing the complete learning process for a DQN on the gym polecart environment to play with"""
 
     ###PARAMETERS###############
     epsilon = .1
@@ -193,7 +271,7 @@ def learn_dqn():
             done = False
 
             while not done:
-                a, Q_sa = pi.select_action(s, policy=policy, epsilon=epsilon, temp=temp)
+                a = pi.select_action(s, policy=policy, epsilon=epsilon, temp=temp)
                 s_next, r, done, _ = env.step(a)
                 if render:
                     env.render()
@@ -208,7 +286,7 @@ def learn_dqn():
 
         # one training iteration
         while not done:
-            a, Q_sa = pi.select_action(s, policy=policy, epsilon=epsilon, temp=temp)
+            a = pi.select_action(s, policy=policy, epsilon=epsilon, temp=temp)
             s_next, r, done, _ = env.step(a)
             if render:
                 env.render()
@@ -251,42 +329,8 @@ def learn_dqn():
     env.close()
 
 
-def random_move(env, render=False):
-    s = env.reset()
-    # rewards = []
-    # states = []
-    rewards = np.full(shape=1000, fill_value=np.nan, dtype=np.float64)
-    states = np.full(shape=1000, fill_value=np.nan, dtype=np.float64)
-    actions = np.full(shape=1000, fill_value=np.nan, dtype=np.float64)
-    current_iter = 0
-
-    states[current_iter] = s
-
-    done = False
-    if render:
-        env.render()
-
-    while not done:
-        a = np.random.choice(env.action_space.n)
-        s, r, done, _ = env.step(a)
-        rewards[current_iter] = r
-        states[current_iter + 1] = s
-        actions[current_iter] = a
-        if render:
-            env.render()
-        time.sleep(0.07)
-        current_iter += 1
-
-    return states, rewards, actions
-
-
 def main():
-    do_random = False
-
-    if do_random:
-        random_move()
-    else:
-        learn_dqn()
+    play_dqn()
 
 
 if __name__ == '__main__':
