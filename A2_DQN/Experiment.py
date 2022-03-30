Fixed mistakes in average_over_repetitions

A2_DQN/Experiment.py
@@ -16,12 +16,12 @@ def average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gam
     for rep in range(n_repetitions): # Loop over repetitions
         rewards = learn_dqn(learning_rate,policy,epsilon,temp,gamma,hidden_layers,use_er,use_tn,num_iterations,depth,learn_freq,\
                   target_update_freq,sample_batch_size=128,anneal_method="linear",render=False)
-
+        
         reward_results[rep] = rewards
         
     print('Running one setting takes {} minutes'.format((time.time()-now)/60))    
     learning_curve = np.mean(reward_results,axis=0) # average over repetitions
-    learning_curve = smooth(learning_curve,smoothing_window) # additional smoothing
+    learning_curve = smooth(learning_curve,window=51) # additional smoothing
     return learning_curve
 
 def main():
@@ -30,14 +30,14 @@ def main():
     learning_rate=0.01
     gamma=0.8
     
-    #epsilon = .1   NOW VARYING OVER EPSILON (see loop below)
+    epsilon = .1
     temp = 1.
-    policy = 'egreedy'  # 'egreedy'
+    policy = 'egreedy'#'softmax'#
     
     hidden_layers = [12,6]
     depth = 250
     batch_size = 128
-    num_iterations = 250
+    num_iterations = 5
     target_update_freq = 25  # iterations
     learn_freq=4
     max_training_batch = int(1e6)  # storage arrays
@@ -52,13 +52,36 @@ def main():
     title = r"Softmax $\tau$=1, +TN -ER"
     ###########################
     
-    Plot = LearningCurvePlot(title = "Deep Q-Learning, Varied over epsilon")
-    n_repetitions = 10
-    for epsilon in [0.1,0.5,0.8]:
-        learning_curve = average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
+    Plot = LearningCurvePlot(title = "Deep Q-Learning, Ablation Study")
+    n_repetitions = 5
+    
+    learning_curve0 = average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
                             use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq)
-        Plot.add_curve(learning_curve,label=r'$\epsilon $ = {}'.format(epsilon))
-    Plot.save('epsilon.png')
-
+    Plot.add_curve(learning_curve0,label=r'DQN')
+    
+    use_er = True
+    use_tn = False
+    learning_curve1 = average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
+                            use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq)
+    Plot.add_curve(learning_curve1,label=r'DQN without TN')
+    
+    use_er = False
+    use_tn = True
+    learning_curve2 = average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
+                            use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq)
+    Plot.add_curve(learning_curve2,label=r'DQN without ER')
+    
+    use_er = False
+    use_tn = False
+    learning_curve3 = average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
+                            use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq)
+    Plot.add_curve(learning_curve3,label=r'DQN without TN and ER')
+    
+    Plot.save('Ablation.png')
+    
+    # Restore previous values
+    use_er = True
+    use_tn = True
+    
 if __name__ == '__main__':
     main()