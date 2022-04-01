e0667c8a6f6e1970d20e588658defdf4cb916b90
B. Silk <silkbha@gmail.com>
2022-04-01 10:37:21 +0000

    Added learning rate study, changed n_repetitions from 5 to 10

--- A2_DQN/Experiment.py
+++ A2_DQN/Experiment.py
@@ -24,7 +24,7 @@ def average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gam
     learning_curve = smooth(learning_curve,window=51) # additional smoothing
     return learning_curve
 
-def main():
+def ablation():
     
     ###PARAMETERS###############
     learning_rate=0.01
@@ -53,7 +53,7 @@ def main():
     ###########################
     
     Plot = LearningCurvePlot(title = "Deep Q-Learning, Ablation Study")
-    n_repetitions = 5
+    n_repetitions = 10
     
     learning_curve0 = average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
                             use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq)
@@ -78,10 +78,45 @@ def main():
     Plot.add_curve(learning_curve3,label=r'DQN without TN and ER')
     
     Plot.save('Ablation.png')
+
+def learn_rate():
     
-    # Restore previous values
+    ###PARAMETERS###############
+    #learning_rate=0.01
+    gamma=0.8
+    
+    epsilon = .1
+    temp = 1.
+    policy = 'egreedy'#'softmax'#
+    
+    hidden_layers = [12,6]
+    depth = 250
+    batch_size = 128
+    num_iterations = 5
+    target_update_freq = 25  # iterations
+    learn_freq=4
+    max_training_batch = int(1e6)  # storage arrays
+    # training_data_shape = (max_training_batch, 1)
+
+    e_anneal = False
     use_er = True
     use_tn = True
+
+    render = False
+    plot = True
+    title = r"Softmax $\tau$=1, +TN -ER"
+    ###########################
+    
+    Plot = LearningCurvePlot(title = "Deep Q-Learning: Various Learning Rates Compared")
+    n_repetitions = 10
+    
+    for learning_rate in [0.01,0.1,0.25,0.5]
+        learning_curve = average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
+                            use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq)
+        Plot.add_curve(learning_curve,label=r'$\alpha$ = {}'.format(learning_rate))
+    Plot.save('Learning_Rates.png')
     
 if __name__ == '__main__':
-    main()
+    #ablation()
+    learn_rate()
+    
