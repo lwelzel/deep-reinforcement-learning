a91674168c370cf68613015ca2a52cda2b98e85c
RenVit318 <rens@renskievit.com>
2022-04-02 22:40:40 +0000

    Fixed plot issue

--- A2_DQN/Experiment2.py
+++ A2_DQN/Experiment2.py
@@ -9,14 +9,21 @@ from dqn import DeepQAgent, learn_dqn
 from helper import LearningCurvePlot, smooth
 
 def average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
-                             use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq,anneal_method):
+                             use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq):
 
     reward_results = np.empty([n_repetitions,num_iterations]) # Result array
     now = time.time()
-    
+    max_epoch_env_steps = 250    
+
     for rep in range(n_repetitions): # Loop over repetitions
-        rewards = run_parallel_dqns(learning_rate,policy,epsilon,temp,gamma,hidden_layers,use_er,use_tn,num_iterations,depth,learn_freq,\
-                  target_update_freq,sample_batch_size=128,anneal_method=None,render=False)
+        rewards = run_parallel_dqns(num_iterations,max_epoch_env_steps,target_update_freq,
+                      policy,
+                      learning_rate, gamma,
+                      epsilon, temp,
+                      hidden_layers, hidden_act='relu', kernel_init='HeUniform',
+                      loss_func='mean_squared_error',
+                      use_tn=use_tn, use_er=use_er,
+                      buffer_type=None, buffer_depth=depth, sample_batch_size=100)
         
         reward_results[rep] = rewards
         
@@ -34,12 +41,11 @@ def main():
     epsilon = .1
     temp = 1.
     policy = 'egreedy'#'softmax'#
-    anneal_method = None
-    
+       
     hidden_layers = [32,32]
     depth = 2500
     batch_size = 128
-    num_iterations = 1000
+    num_iterations = 500
     target_update_freq = 25  # iterations
     learn_freq=4
     max_training_batch = int(1e6)  # storage arrays
@@ -55,7 +61,7 @@ def main():
 
     do_ablation = False
     do_exploration = True
-    do_layers = False
+    do_layers = True
     ###########################
     n_repetitions = 5    
 
@@ -94,33 +100,30 @@ def main():
 
 
     if do_exploration:
-        eg_exp_ar = [[0.01,None],[0.1,None],[0.25,None],[1.,'linear']]
-        sm_exp_ar = [0.01,0.1,1.]
-        for epsilon,anneal_method in exp_ar:
+        Plot = LearningCurvePlot(title = "Deep Q-Learning, Exploration Study")
+        eg_exp_ar = [0.01,0.1,0.25,1.]
+
+        for epsilon in eg_exp_ar:
             lc = average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
-                        use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq,anneal_method)
-            if anneal_method == None:
-                label = r"$\epsilon$ = "+str(epsilon)
-            else:
-                label = r"$\epsilon$-greedy Linear Annealing"
-            Plot.add_curve(lc,label=label)
+                        use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq)
+            Plot.add_curve(lc,label=r"$\epsilon$ = "+str(epsilon))
 
         policy = 'softmax'
-        for temp in sm_exp_ar:
-            lc = average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
-            use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq,anneal_method)
-            Plot.add_curve(lc,label=r'$\tau$ = '+str(temp)      
+        lc = average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
+        use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq)
+        Plot.add_curve(lc,label=r'$\tau$ = '+str(temp))
 
         Plot.save('exploration.png')
         
         policy = 'egreedy'
-        epsilon, temp, anneal_method = 0.1, 1., None
+        epsilon, temp = 0.1, 1.
 
     if do_layers:
-        layers_ar = [[64],[48,24],[32,32]]
+        Plot = LearningCurvePlot(title = "Deep Q-Learning, Hidden Layer Study")
+        layers_ar = [[24,12],[48,24],[32,32]]
         for hidden_layers in layers_ar:
             lc = average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
-            use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq,anneal_method)
+            use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq)
             Plot.add_curve(lc,label=hidden_layers)
         Plot.save('archictecture.png')
 
