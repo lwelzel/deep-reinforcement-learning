bb0fff1560da29740bc416a630fb0c0f0b3f4e2d
B. Silk <silkbha@gmail.com>
2022-04-04 14:13:46 +0000

    Merged with Experiment2.py, Cleaned up

--- A2_DQN/Experiment.py
+++ A2_DQN/Experiment.py
@@ -4,41 +4,43 @@
 import numpy as np
 import time
 
+from many_dqn_wrapper import run_parallel_dqns
 from dqn import DeepQAgent, learn_dqn
-from helper import LearningCurvePlot, smooth
 
 def average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
                              use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq):
-
-    reward_results = np.empty([n_repetitions,num_iterations]) # Result array
-    now = time.time()
     
+    now = time.time()
+    max_epoch_env_steps = 200
+
     for rep in range(n_repetitions): # Loop over repetitions
-        rewards = learn_dqn(learning_rate,policy,epsilon,temp,gamma,hidden_layers,use_er,use_tn,num_iterations,depth,learn_freq,\
-                  target_update_freq,sample_batch_size=128,anneal_method="linear",render=False)
-        
-        reward_results[rep] = rewards
+        run_parallel_dqns(num_iterations,max_epoch_env_steps,target_update_freq,
+                      policy,
+                      learning_rate, gamma,
+                      epsilon, temp,
+                      hidden_layers, hidden_act='relu', kernel_init='HeUniform',
+                      loss_func='mean_squared_error',
+                      use_tn=use_tn, use_er=use_er,
+                      buffer_type=None, buffer_depth=depth, sample_batch_size=100)
         
     print('Running one setting takes {} minutes'.format((time.time()-now)/60))    
-    learning_curve = np.mean(reward_results,axis=0) # average over repetitions
-    learning_curve = smooth(learning_curve,window=51) # additional smoothing
-    return learning_curve
+    return
 
-def ablation():
+def main():
     
     ###PARAMETERS###############
     learning_rate=0.01
-    gamma=0.8
+    gamma=0.9
     
-    epsilon = .1
+    epsilon = 1. #starting epsilon, annealing
     temp = 1.
     policy = 'egreedy'#'softmax'#
-    
-    hidden_layers = [12,6]
-    depth = 250
+       
+    hidden_layers = [512,256,64]
+    depth = 2000
     batch_size = 128
-    num_iterations = 500
-    target_update_freq = 25  # iterations
+    num_iterations = 250
+    target_update_freq = 4  # iterations
     learn_freq=4
     max_training_batch = int(1e6)  # storage arrays
     # training_data_shape = (max_training_batch, 1)
@@ -49,74 +51,42 @@ def ablation():
 
     render = False
     plot = True
-    title = r"Softmax $\tau$=1, +TN -ER"
+
+    do_exploration = False
+    do_layers = False
+    do_learnrates = True
+    do_gammas = False
     ###########################
+    n_repetitions = 1   
     
-    Plot = LearningCurvePlot(title = "Deep Q-Learning, Ablation Study")
-    n_repetitions = 10
     
-    learning_curve0 = average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
-                            use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq)
-    Plot.add_curve(learning_curve0,label=r'DQN')
+    if do_learnrates:
+        for learning_rate in [0.25,0.5]:
+            average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
+                                use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq)
+        learning_rate=0.01
     
-    use_er = True
-    use_tn = False
-    learning_curve1 = average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
-                            use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq)
-    Plot.add_curve(learning_curve1,label=r'DQN without TN')
+    if do_gammas:        
+        for gamma in [0.1,0.3,0.6]:
+            average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
+                                use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq)
+        gamma=0.9
     
-    use_er = False
-    use_tn = True
-    learning_curve2 = average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
-                            use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq)
-    Plot.add_curve(learning_curve2,label=r'DQN without ER')
-    
-    use_er = False
-    use_tn = False
-    learning_curve3 = average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
-                            use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq)
-    Plot.add_curve(learning_curve3,label=r'DQN without TN and ER')
-    
-    Plot.save('Ablation.png')
+    if do_exploration:
+        policy = 'softmax'
+        average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
+                                 use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq)
 
-def learn_rate():
-    
-    ###PARAMETERS###############
-    #learning_rate=0.01
-    gamma=0.8
-    
-    epsilon = .1
-    temp = 1.
-    policy = 'egreedy'#'softmax'#
-    
-    hidden_layers = [12,6]
-    depth = 250
-    batch_size = 128
-    num_iterations = 500
-    target_update_freq = 25  # iterations
-    learn_freq=4
-    max_training_batch = int(1e6)  # storage arrays
-    # training_data_shape = (max_training_batch, 1)
+        policy = 'egreedy'
+        average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
+                                 use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq)
 
-    e_anneal = False
-    use_er = True
-    use_tn = True
+    if do_layers:
+        layers_ar = [[64,32],[128,64],[256,64],[256,128,64],[512,256,64]]
+        for hidden_layers in layers_ar:
+            average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
+                                     use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq)
+        hidden_layers = [512,256,64]
 
-    render = False
-    plot = True
-    title = r"Softmax $\tau$=1, +TN -ER"
-    ###########################
-    
-    Plot = LearningCurvePlot(title = "Deep Q-Learning: Various Learning Rates Compared")
-    n_repetitions = 10
-    
-    for learning_rate in [0.01,0.1,0.25,0.5]:
-        learning_curve = average_over_repetitions(n_repetitions,learning_rate,policy,epsilon,temp,gamma,hidden_layers,\
-                            use_er,use_tn,num_iterations,depth,learn_freq,target_update_freq)
-        Plot.add_curve(learning_curve,label=r'$\alpha$ = {}'.format(learning_rate))
-    Plot.save('Learning_Rates.png')
-    
 if __name__ == '__main__':
-    #ablation()
-    learn_rate()
-    
+    main()
