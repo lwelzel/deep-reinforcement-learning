509e142ee529bcc3f91f665aaa742b44e3ea67ba
B. Silk <silkbha@gmail.com>
2022-04-04 13:56:09 +0000

    Update A2_DQN/plot_rewards.py

--- A2_DQN/plot_rewards.py
+++ A2_DQN/plot_rewards.py
@@ -12,7 +12,7 @@ def read_h5_reward_file(loc):
 
     return rewards, header
 
-def read_all_rewards(dir, fig="rewards", plot_all_paths=True):
+def read_all_rewards(dir, fig="rewards", plot_all_paths=False):
     files = Path(dir).rglob(f"*Rewards*.h5")
     files = np.array([path for path in files]).flatten()
     rewards = np.ones((len(files), 5000))
@@ -37,9 +37,9 @@ def read_all_rewards(dir, fig="rewards", plot_all_paths=True):
 
     rewards = rewards[:, :min_len]
 
-    label = f"example_label (n={len(headers)})"
+    #label = f"example_label (n={len(headers)})"
 
-    return rewards, label
+    return rewards, len(headers)
 
 def plot_rewards_batch(rewards, label, window=21, sigma=1, fig="rewards"):
     fig = plt.figure(num=fig)
@@ -56,8 +56,7 @@ def plot_rewards_batch(rewards, label, window=21, sigma=1, fig="rewards"):
     ax.fill_between(np.arange(len(smooth_mean)),
                     np.clip(smooth_mean + smooth_std * sigma, a_min=0., a_max=None),
                     np.clip(smooth_mean - smooth_std * sigma, a_min=0., a_max=None),
-                    alpha=0.1,
-                    label=f"{sigma} "r"$\sigma$ CI")
+                    alpha=0.1)#,label=f"{sigma} "r"$\sigma$ CI")
 
 def plot_rewards_comparison(*args):
     fig, ax = plt.subplots(num="rewards",
@@ -81,8 +80,84 @@ def plot_rewards_comparison(*args):
     return
 
 
+def plot_rewards_comparison_alpha(*args):
+    fig, ax = plt.subplots(num="rewards",
+                           nrows=1, ncols=1,
+                           constrained_layout=True,
+                           figsize=(9, 6))
+    rewards, n = read_all_rewards(Path("BATCHES/batch=2022-04-03-16-07-43_a=1e-02_g=9e-01_hlay=(512, 256, 64)"))
+    label = f"$\\alpha = 0.01$ (n={n})"
+    plot_rewards_batch(rewards, label)
+    
+    rewards, n = read_all_rewards(Path("BATCHES/batch=2022-04-03-23-45-18_a=1e-01_g=9e-01_hlay=(512, 256, 64)"))
+    label = f"$\\alpha = 0.1$ (n={n})"
+    plot_rewards_batch(rewards, label)
+    
+    rewards, n = read_all_rewards(Path("BATCHES/batch=2022-04-04-09-06-35_a=2e-01_g=9e-01_hlay=(512, 256, 64)"))
+    label = f"$\\alpha = 0.25$ (n={n})"
+    plot_rewards_batch(rewards, label)
+    
+    rewards, n = read_all_rewards(Path("batch=2022-04-04-12-04-15_a=5e-01_g=9e-01_hlay=(512, 256, 64)"), plot_all_paths=True)
+    label = f"$\\alpha = 0.5$ (n={n})"
+    plot_rewards_batch(rewards, label)
+    
+    ax.set_ylim(0., None)
+    ax.set_xlim(0., 250)
+    
+    ax.axhline(200, ls='--', c='gray', label="200 Reward Limit")
+    ax.set_xlabel('Episode [-]')
+    ax.set_ylabel('Mean reward [-]')
+    ax.set_title(f'DQN Rewards during Training: Learning Rates Comparison')
+    ax.legend()
+
+    # fig.suptitle('example sup title', fontsize=16)
+
+    #plt.show()
+    plt.savefig("learning_rates.png")
+    plt.close()
+    return
+
+
+def plot_rewards_comparison_gamma(*args):
+    fig, ax = plt.subplots(num="rewards",
+                           nrows=1, ncols=1,
+                           constrained_layout=True,
+                           figsize=(9, 6))
+    rewards, n = read_all_rewards(Path("BATCHES/batch=2022-04-04-09-09-18_a=1e-02_g=1e-01_hlay=(512, 256, 64)"))
+    label = f"$\\gamma = 0.1$ (n={n})"
+    plot_rewards_batch(rewards, label)
+    
+    rewards, n = read_all_rewards(Path("BATCHES/batch=2022-04-04-09-55-11_a=1e-02_g=3e-01_hlay=(512, 256, 64)"))
+    label = f"$\\gamma = 0.3$ (n={n})"
+    plot_rewards_batch(rewards, label)
+    
+    rewards, n = read_all_rewards(Path("BATCHES/batch=2022-04-04-11-13-44_a=1e-02_g=6e-01_hlay=(512, 256, 64)"))
+    label = f"$\\gamma = 0.6$ (n={n})"
+    plot_rewards_batch(rewards, label)
+    
+    rewards, n = read_all_rewards(Path("BATCHES/batch=2022-04-03-16-07-43_a=1e-02_g=9e-01_hlay=(512, 256, 64)"))
+    label = f"$\\gamma = 0.9$ (n={n})"
+    plot_rewards_batch(rewards, label)
+    
+    ax.set_ylim(0., None)
+    ax.set_xlim(0., 250)
+    
+    ax.axhline(200, ls='--', c='gray', label="200 Reward Limit")
+    ax.set_xlabel('Episode [-]')
+    ax.set_ylabel('Mean reward [-]')
+    ax.set_title(f'DQN Rewards during Training: Discount Factors Comparison')
+    ax.legend()
+
+    # fig.suptitle('example sup title', fontsize=16)
+
+    #plt.show()
+    plt.savefig("discount_factors.png")
+    plt.close()
+    return
+
 def main():
-    plot_rewards_comparison()
+    plot_rewards_comparison_alpha()
+    plot_rewards_comparison_gamma()
 
 
 if __name__ == '__main__':
