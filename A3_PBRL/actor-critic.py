372f177bb4af978858c2e5becabbd1dcaf89ae4c
RenVit318 <rens@renskievit.com>
2022-04-22 09:45:06 +0000

    template

---
+++ A3_PBRL/actor-critic.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#######################
+#
+#  Implementation of the actor-critic approach
+#  includes options for addition of bootstrapping 
+#  and baseline subtraction (probably ?)
+#
+#  IMPORTANT FUNCTIONS
+#   
+#  
+#
+#
+#######################
+
+
+
+
+
+
+
+def main():
+    pass;
+
+
+if __name__ == '__main__':
+    main()
