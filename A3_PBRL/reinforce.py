corrected gradient normalizer to num_traces

A3_PBRL/reinforce.py
@@ -16,8 +16,8 @@ class ReinforceAgent(BaseAgent):
     def __init__(self, state_space, action_space, **kwargs):
         super().__init__(state_space, action_space, name='reinforce', **kwargs)
 
-    def update_policy(self, trace_array, episode_len, train_length):
-        """"Reinforce weights update function as described by Algorithm """
+    def update_policy(self, trace_array, episode_len):
+        """"Reinforce weights update function as described by Algorithm ??"""
         weight_grad = np.zeros_like(self.network.get_weights(), dtype=object)
 
         num_traces = trace_array.shape[0]
@@ -30,22 +30,23 @@ class ReinforceAgent(BaseAgent):
                 a, r = int(trace[(4 * t) + 1][0]), int(trace[(4 * t) + 2][0])
 
                 cumu_trace_reward = r + self.discount * cumu_trace_reward
-                weight_grad += self.one_step_weight_grad(s, a, cumu_trace_reward, train_length)
+                weight_grad += self.one_step_weight_grad(s, a, cumu_trace_reward, num_traces)
 
 
         self.update_weights(weight_grad)
 
-    def one_step_weight_grad(self, s, a, cumu_trace_reward, train_length):
+    def one_step_weight_grad(self, s, a, cumu_trace_reward, num_traces):
         """Computes the one step gradient update"""
         x = tf.constant(s, shape=(1, 4))
         with tf.GradientTape() as tape:
             val_sa = self.network(x)[0][a]
             log_val_sa = tf.math.log(val_sa)
         grad_log = tape.gradient(log_val_sa, self.network.trainable_variables)
-        grad_log = [(cumu_trace_reward/train_length) * item.numpy() for item in grad_log]
+        grad_log = [(cumu_trace_reward/num_traces) * item.numpy() for item in grad_log]
 
         return grad_log
 
+    #def update_with_loss(self, trace_array, episode_len):
 
     # def loss_function(self, trace_array, episode_len):
     #    """Full loss function to push through automatic differentiation"""

A3_PBRL/training_functions.py
@@ -26,6 +26,8 @@ def sample_traces(env, pi, n_traces, verbose=False):
         while not done:
             a = pi.select_action(s)
             s_next, r, done, _ = env.step(a)
+            if t == pi.max_reward:
+                done = True
 
             transition = np.zeros((4, 4))
             for j, obs in enumerate([s, a, r, s_next]):
@@ -34,8 +36,8 @@ def sample_traces(env, pi, n_traces, verbose=False):
 
             s = s_next
             t += 1
-            if verbose: env.render()
-            if t == pi.max_reward: done = True
+            if verbose:
+                env.render()
 
         episode_len[i] = t
 
@@ -44,7 +46,7 @@ def sample_traces(env, pi, n_traces, verbose=False):
 
 def train(method, train_length=100, n_traces=5, verbose=False,
           save_rewards=False, save_freq=10, **kwargs):
-    """Training function"""
+    """General Training function"""
 
     env = gym.make('CartPole-v1')
     if method == 'reinforce':
@@ -54,9 +56,9 @@ def train(method, train_length=100, n_traces=5, verbose=False,
 
     average_trace_reward = np.zeros(train_length)
     for epoch in range(train_length):
-        t0 = time.time()
+        t_start = time.time()
         trace_array, episode_len = sample_traces(env, pi, n_traces, verbose)
-        pi.update_policy(trace_array, episode_len, train_length)
+        pi.update_policy(trace_array, episode_len)
         average_trace_reward[epoch] = np.mean(episode_len)
 
         if (epoch % save_freq) == 0 and save_rewards:
@@ -65,10 +67,10 @@ def train(method, train_length=100, n_traces=5, verbose=False,
         if verbose:
             if pi.exp_policy == 'egreedy':
                 exp_factor = pi.epsilon
-            if pi.exp_policy == 'softmax':
+            elif pi.exp_policy == 'softmax':
                 exp_factor = pi.temp
             print(
-                f"Completed Iteration {epoch} | Elapsed Time: {time.time() - t0:.2f}s | Mean Reward: {average_trace_reward[epoch]} | Exploration Factor: {exp_factor:.2f} ({pi.exp_policy})")
+                f"Completed Iteration {epoch} | Elapsed Time: {time.time() - t_start:.2f}s | Mean Reward: {average_trace_reward[epoch]} | Exploration Factor: {exp_factor:.2f} ({pi.exp_policy})")
 
         pi.anneal_policy_parameter(epoch, train_length)
     if save_rewards: